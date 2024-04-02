7c725a80ef275799016327f0aff0c871e307aae0
Thassio <thassio.vagner1@gmail.com>
2024-04-02 20:12:53 -0300

    feat: update language

--- ExprParser.py
+++ ExprParser.py
@@ -10,7 +10,7 @@ else:
 
 def serializedATN():
     return [
-        4,1,32,169,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,2,4,7,4,2,5,7,5,2,6,7,
+        4,1,33,169,2,0,7,0,2,1,7,1,2,2,7,2,2,3,7,3,2,4,7,4,2,5,7,5,2,6,7,
         6,2,7,7,7,2,8,7,8,2,9,7,9,2,10,7,10,2,11,7,11,2,12,7,12,2,13,7,13,
         2,14,7,14,2,15,7,15,2,16,7,16,2,17,7,17,2,18,7,18,2,19,7,19,1,0,
         1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1,1,3,1,51,8,1,1,2,1,2,1,2,1,2,1,
@@ -89,7 +89,7 @@ class ExprParser ( Parser ):
                       "TRUE", "WRITE", "IF", "THEN", "ELSE", "INT", "OPAD", 
                       "OPMULT", "OPREL", "OPLOG", "OPNEG", "PVIG", "PONTO", 
                       "DPONTOS", "VIG", "ABPAR", "FPAR", "ATRIB", "CADEIA", 
-                      "WS", "ID" ]
+                      "WS", "ID", "CTE" ]
 
     RULE_prog = 0
     RULE_decls = 1
@@ -150,6 +150,7 @@ class ExprParser ( Parser ):
     CADEIA=30
     WS=31
     ID=32
+    CTE=33
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
